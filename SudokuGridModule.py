d2f9d20ef3d4d5170f30e3776fe1e22eb2e83d41
Hugh Shin <hughbee135@gmail.com>
2020-12-27 16:42:05 -0800

    12/27 session

---
+++ SudokuGridModule.py
@@ -0,0 +1,16 @@
+class SudokuGrid:
+
+  # Constructor
+  def __init__(self):
+    self.grid = [[0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+  # Method that modifies a square
+  def Write(self, row, col, value):
+    self.grid[row][col] = value
+
+  # Method that prints the grid
+  def Print(self):
+    print(self.grid)
+
+  def SolveItself(self):
+    # TODO
--- main.py
+++ main.py
@@ -1,19 +1,9 @@
+# Entry point
 
+import SudokuGridModule
 
-# Pay Program
+sudokuGrid = SudokuGridModule.SudokuGrid()
+print(sudokuGrid.grid)
 
-try:
-    hours_worked = float(input("Enter number of hours worked: "))
-    rate = float(input("Enter rate: ")) * 1.5
-    total_pay = hours_worked * rate
-    if hours_worked > 40:
-        total_pay = hours_worked * rate
-    else:
-        total_pay = hours_worked * 10
-    print("Pay: " + str(total_pay))
-
-except:
-    print("Please start over and enter a number")
-
-
-
+sudokuGrid.Write(3, 6, 2)
+sudokuGrid.Print()
